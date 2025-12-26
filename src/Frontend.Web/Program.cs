591a34801644469d490effc4d8ed828e790d5011
ExamenFe AutoBot <auto@examenfe.dev>
2025-12-26 21:05:23 +0000

    refactor: set Profesores as default landing page
    - Changed default route from Home/Index to Profesores/Index
    - Updated error handler to redirect to Profesores/Index instead of Home/Error
    - Home page is no longer used as entry point
    - Build: 0 errors, 0 warnings

--- src/Frontend.Web/Program.cs
+++ src/Frontend.Web/Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Profesores/Index");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Profesores}/{action=Index}/{id?}");
 
 app.Run();
